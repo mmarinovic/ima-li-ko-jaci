788f936ff673e625fc1f8ff88b424a55c4327399
mmarinovic <mmarinovic@neogov.net>
2014-10-12 01:53:22 +0200

    add filter

---
+++ KisaMetaka.ImaLiKoJaci.Web/Scripts/src/filters/dateTime.js
@@ -0,0 +1,23 @@
+﻿imaLiJaciModul
+    .filter('dateTime', ['$filter', function($filter) {
+
+        return function(dateTime) {
+          
+            if (dateTime) {
+
+                return $filter('date')(dateTime, 'yyyy-MM-dd HH:mm:ss');
+            }
+        };
+    }]);
+
+imaLiJaciModul
+    .filter('timeFilter', ['$filter', function ($filter) {
+
+        return function (dateTime) {
+
+            if (dateTime) {
+
+                return $filter('date')(dateTime, 'HH:mm:ss');
+            }
+        };
+    }]);
