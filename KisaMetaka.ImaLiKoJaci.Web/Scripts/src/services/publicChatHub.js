ce899813a46f35a2475396063068eba971fafeb1
mmarinovic <mmarinovic@neogov.net>
2014-10-12 01:31:38 +0200

    applied

--- KisaMetaka.ImaLiKoJaci.Web/Scripts/src/services/publicChatHub.js
+++ KisaMetaka.ImaLiKoJaci.Web/Scripts/src/services/publicChatHub.js
@@ -1,20 +1,22 @@
 ﻿imaLiJaciModul
-    .factory('publicChatHub', ['Hub', '$rootScope', 'hubListenerNames', function (Hub, $rootScope, hubListenerNames) {
+    .factory('publicChatHub', ['Hub', '$rootScope', 'hubListenerNames', '$timeout', function (Hub, $rootScope, hubListenerNames, $timeout) {
 
     return new Hub('publicChat', {
         
         listeners: {
             ShowNewQuestion: function (question) {
-
                 $rootScope.$broadcast(hubListenerNames.showNewQuestion, question);
+                $rootScope.$apply();
             },
             ShowCorrectAnswer: function(correctAnswer) {
                 
                 $rootScope.$broadcast(hubListenerNames.showCorrectAnswer, correctAnswer);
+                $rootScope.$apply();
             },
             ShowAnswer: function (answer) {
                 
                 $rootScope.$broadcast(hubListenerNames.showAnswer, answer);
+                $rootScope.$apply();
             }
         },
         errorHandler: function (error) {
