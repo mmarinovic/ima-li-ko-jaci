fa7f54c39592cd2720ff1bc5956d6997591c5f0a
Nebojsa Veron <veron.nebojsa@gmail.com>
2014-10-11 23:51:39 +0200

    Dependency config and signalr config

---
+++ KisaMetaka.ImaLiKoJaci.Web/App_Start/DependencyConfig.cs
@@ -0,0 +1,21 @@
+﻿using StructureMap;
+
+namespace KisaMetaka.ImaLiKoJaci.Web
+{
+    public class DependecyConfig
+    {
+        public static void RegisterDependencies()
+        {
+            ObjectFactory.Initialize(x =>
+            {
+                x.IncludeRegistry<Infrastructure.DependencyRegistry>();
+
+                x.Scan(scanner =>
+                {
+                    scanner.Assembly("KisaMetaka.ImaLiKoJaci.Infrastructure");
+                    scanner.SingleImplementationsOfInterface();
+                });
+            });
+        }
+    }
+}
---
+++ KisaMetaka.ImaLiKoJaci.Web/App_Start/SignalRConfig.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(KisaMetaka.ImaLiKoJaci.Web.SignalRConfig))]
+namespace KisaMetaka.ImaLiKoJaci.Web
+{
+    public class SignalRConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
