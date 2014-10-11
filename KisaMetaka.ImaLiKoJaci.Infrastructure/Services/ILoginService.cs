2425735bd373b1a4733ccb19ea7f937f505c04ca
Nebojsa Veron <veron.nebojsa@gmail.com>
2014-10-11 23:15:59 +0200

    Login service

---
+++ KisaMetaka.ImaLiKoJaci.Infrastructure/Services/ILoginService.cs
@@ -0,0 +1,12 @@
+﻿using KisaMetaka.ImaLiKoJaci.Infrastructure.User;
+
+namespace KisaMetaka.ImaLiKoJaci.Infrastructure.Services
+{
+    public interface ILoginService
+    {
+        UserDto TryGetSignedInUser();
+
+        void SignIn(string facebookId);
+        void SignOut();
+    }
+}
---
+++ KisaMetaka.ImaLiKoJaci.Infrastructure/Services/LoginService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Web;
+using System.Web.Security;
+using KisaMetaka.ImaLiKoJaci.Infrastructure.User;
+
+namespace KisaMetaka.ImaLiKoJaci.Infrastructure.Services
+{
+    public class LoginService : ILoginService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public LoginService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public UserDto TryGetSignedInUser()
+        {
+            var facebookId = HttpContext.Current.User.Identity.Name;
+            if (string.IsNullOrWhiteSpace(facebookId)) { return null; }
+
+            var user = _userRepository.TryGet(facebookId);
+            if (user == null) { return null; }
+
+            return user;
+        }
+
+        public void SignIn(string facebookId)
+        {
+            if (string.IsNullOrWhiteSpace(facebookId)) { throw new ArgumentException("facebookId"); }
+
+            FormsAuthentication.SetAuthCookie(facebookId, true);
+        }
+
+        public void SignOut()
+        {
+            FormsAuthentication.SignOut();
+        }
+    }
+}
