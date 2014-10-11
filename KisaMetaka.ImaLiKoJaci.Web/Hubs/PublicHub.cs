95a204e029d06184f03dcd0843d948a6b83bb26d
Nebojsa Veron <veron.nebojsa@gmail.com>
2014-10-11 23:35:15 +0200

    SignalR and hub

---
+++ KisaMetaka.ImaLiKoJaci.Web/Hubs/PublicHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace KisaMetaka.ImaLiKoJaci.Web.Hubs
+{
+    [HubName("publicChat")]
+    public class PublicHub : Hub
+    {
+        public static void ShowNewQuestion(string question)
+        {
+            var hubContext = GlobalHost.ConnectionManager.GetHubContext<PublicHub>();
+            hubContext.Clients.All.showNewQuestion(question);
+        }
+
+        public static void ShowCorrectAnswer(string winnerDisplayName, string answer)
+        {
+            var hubContext = GlobalHost.ConnectionManager.GetHubContext<PublicHub>();
+            hubContext.Clients.All.showCorrectAnswer(winnerDisplayName, answer);
+        }
+
+        public static void SendAnswer(string userFacebookId, string answer)
+        {
+            var hubContext = GlobalHost.ConnectionManager.GetHubContext<PublicHub>();
+            hubContext.Clients.All.sendAnswer(userFacebookId, answer);
+        }
+    }
+}
