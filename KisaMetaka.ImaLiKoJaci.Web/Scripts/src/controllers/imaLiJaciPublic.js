bb4bb950c5b6e401b5fd4363b0adf0f8e7625f27
Nebojsa Veron <veron.nebojsa@gmail.com>
2014-10-12 15:16:21 +0200

    Text changes

--- KisaMetaka.ImaLiKoJaci.Web/Scripts/src/controllers/imaLiJaciPublic.js
+++ KisaMetaka.ImaLiKoJaci.Web/Scripts/src/controllers/imaLiJaciPublic.js
@@ -1,7 +1,5 @@
 ﻿imaLiJaciModul.controller('imaLiJaciPublic', ['$scope', 'publicChatHub', 'hubListenerNames', function ($scope, publicChatHub, hubListenerNames) {
-
-    $scope.message = "Ima li tko jaci?";
-
+    
     $scope.$on(hubListenerNames.showCorrectAnswer, function(e, correctAnswer) {
 
         console.log(correctAnswer);
